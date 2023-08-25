b9548b66b145700a07f54f1caecee713eae9b3d9
juan pablo mendes <jp436807@gmail.com>
2023-08-24 22:16:08 -0300

    criando o hero e importando a font

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({outputStyle : 'compressed'}))
     .pipe(gulp.dest('./dist/css'));
 }
-exports.default = styles;
+
+function images(){
+    return gulp.src('./editarimg/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/editarimg'));
+}
+
+exports.default = gulp.parallel(styles,images);
+
 exports.watch = function(){
 gulp.watch('./src/styles/*.scss', gulp.parallel(styles))}
